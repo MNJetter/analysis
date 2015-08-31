e5f96f412f52a0d927e832c9e7ad41f8238de1e8
MNJetter <alywork@Alysons-MacBook-Air.local>
2015-08-31 10:02:22 +0900

    A bit of cleanup
    Simplified instruction text and changed input commands to raw_input
    commands

--- nconcord.py
+++ nconcord.py
@@ -8,7 +8,7 @@
 ## as your target file to make this work!       ##
 ##                                              ##
 ## Please note that you will also need to have  ##
-## NLTK 3 installed. Find installation          ##
+## NLTK v3 installed. Find installation         ##
 ## instructions at http://www.nltk.org/         ##
 ##################################################
 ## Written by A. Woolfrey                       ##
@@ -17,37 +17,16 @@
 # get the nltk tools
 import nltk
 
-# ask the user for a file name 
-print """
-
-WARNING
-Make sure that you are in the same directory 
-as your target file to make this work!
-When you enter your file:
-- include the file suffix.
-- put the file name in single quotes
-Example: 'room.txt'
-
----------------
-"""
-file = open(str(input("File name: ")))
+# ask the user for a file name
+fname = open(str(raw_input("File name: ")))
 
 # turn the file into an NLTK tokenized object
-t = file.read();
+t = fname.read();
 tokens = nltk.word_tokenize(t)
 text = nltk.Text(tokens)
 
 # ask the user for a word
-print """
-Which word would you like to explore?
-(Don't forget the single quotes!)
-"""
-textword = str(input("Word: "))
+textword = str(raw_input("Word: "))
 
 # get a concordance of the word
-print """
-
----------------
-
-"""
 text.concordance(textword, lines=1000)
