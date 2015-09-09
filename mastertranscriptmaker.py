dca0f16250bfd7fb1b37b8aa2cac7b03ce381d90
MNJetter <alywork@Alysons-MacBook-Air.local>
2015-09-09 10:09:34 +0900

    Cleaned up and commented

--- mastertranscriptmaker.py
+++ mastertranscriptmaker.py
@@ -1,27 +1,38 @@
-# get regex module and prepare the search function we're going to be using. The search function basically grabs any lowercase or capital letter.
-import re
-result = re.compile('[a-zA-Z]+')
-
-######## old stuff from single file tests ########
-## f = open('3m-davidfrazee-int-e.srt','r')     ##
-## material = f.read()                          ##
-## master = open('MASTERTRANSCRIPT.txt','w')    ##
-## for subline in material.splitlines():        ##
-##     if result.match(subline):                ##
-##         master.write(subline + '\n')         ##
-## f.close()                                    ##
-## master.close()                               ##
+##################################################
+## TRANSCRIPTER                                 ##
+##################################################
+## This script, working from ~/analysis/, takes ##
+## all the SRT files from ~/interview_srts/ and ##
+## makes a single text file of the EN lines.    ##
+##################################################
+## (c) 2015 Aly Woolfrey                        ##
+## Licensed for unlimited use, modification, or ##
+## distribution in perpetuity to SMATOOS, Inc.  ##
 ##################################################
 
-# TODO: make it work with a directory!
-master = open('MASTERTRANSCRIPT.txt','w')
+import re
 import os, glob
-path = './'
-for infile in glob.glob( os.path.join(path, '*.srt') ):
-    f = open(infile,'r')
+
+# regex function that asks for any capital or lowercase letters
+result = re.compile('[a-zA-Z]+')
+
+# tells the computer where to look for SRT files (target directory)
+path = '../interview_srts/'
+
+# makes an empty master transcript file in the target directory
+master = open(path + 'MASTERTRANSCRIPT.txt','w')
+
+# reads all SRTs in the target directory
+for stuff in glob.glob(os.path.join(path, '*.srt')):
+    f = open(stuff,'r')
     material = f.read()
+# finds EN lines with the regex function and writes them to the master file
     for subline in material.splitlines():
     	if result.match(subline):
     		master.write(subline + '\n')
    	f.close()
 master.close()
+
+##################################################
+## End TRANSCRIPTER                             ##
+##################################################
