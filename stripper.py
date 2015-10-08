53c63973feffd38615d04ffd94f0684afc5fccec
MNJetter <mnjetter@gmail.com>
2015-10-08 12:31:45 +0900

    Stripper script!
    This will pick out the English lines from an SRT file and copy them to a TXT file.

---
+++ stripper.py
@@ -0,0 +1,38 @@
+##################################################
+## Begin STRIPPER script.                       ##
+##################################################
+## This is a script to take all of the          ##
+## English-language SRT files in a directory,   ##
+## strip them of their timestamps, and re-save  ##
+## them as TXT files for the purpose of         ##
+## language analysis.                           ##
+##################################################
+## (c) 2015 SMATOOS, Inc.                       ##
+##################################################
+
+# import Regex and set up the basic search criteria
+import re
+english = re.compile('[a-zA-Z]')
+
+# define the SRT to be read from
+filename = 's136-in-the-office---greeting'
+
+# open up the SRT to be read from
+oldfile = open(filename + '.srt','r')
+oldfile_content = oldfile.read()
+
+# make a TXT file to write the transcript to
+newfile = open(filename + '---transcript.txt','w')
+
+# write the transript lines
+for line in oldfile_content.splitlines():
+	if english.match(line):
+		newfile.write(line + '\n')
+
+# close out the files
+newfile.close()
+oldfile.close()
+
+##################################################
+## End STRIPPER script.                         ##
+##################################################
